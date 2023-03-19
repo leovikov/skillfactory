b79df93e8a7941d44b516ca8da4179fc283104f6
Leonid <leovikov@gmail.com>
2023-03-19 17:47:28 +0300

    second_final_commit

--- guess-number-task/game_task.py
+++ guess-number-task/game_task.py
@@ -4,77 +4,72 @@ import numpy as np
 
 # Функция, которая угадает number
 def game_predict(number):
-    """Угадываем число
+    """Функция угадывающая число от 1 до 100.
+    Функция учитывает информацию о том, больше или меньше случайное число нужного нам числа.
 
     Args:
         number: Загаданное компьютером число
 
     Returns:
-        int: Количество попыток
+        count: Количество попыток
     """
-    count = 0
+    count = 0 # Задаём переменную
 
-    while True:
+    # Сначала пробуем среднее значение, чтобы сократить зону поиска
+    predict_number = 50
+    count += 1
+    if number == predict_number:
+        return count
+        
+    # Если загаданное число меньше 50
+    elif number < predict_number:
+        # Таким же способом далее сокращаем зону поиска
+        predict_number = 25
         count += 1
-        
-        if number in range(1, 11):
-            predict_number = np.random.randint(1, 11)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(11, 21):
-            predict_number = np.random.randint(11, 21)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(21, 31):
-            predict_number = np.random.randint(21, 31)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(31, 41):
-            predict_number = np.random.randint(31, 41)
-            if number == predict_number:
-                break  # Угадано
+        if number == predict_number:
+            return count # Возвращаем текущее количество попыток если число угадано
             
-        elif number in range(41, 51):
-            predict_number = np.random.randint(41, 51)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(51, 61):
-            predict_number = np.random.randint(51, 61)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(61, 71):
-            predict_number = np.random.randint(61, 71)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(71, 81):
-            predict_number = np.random.randint(71, 81)
-            if number == predict_number:
-                break  # Угадано
-        
-        elif number in range(81, 91):
-            predict_number = np.random.randint(81, 91)
-            if number == predict_number:
-                break  # Угадано
-        
+        elif number < predict_number:
+            # Зададим список из оставшихся возможных вариантов чисел и перебираем каждое 
+            predict_list = list(range(1, 25))
+            for predict_number in predict_list:
+                count += 1
+                if number == predict_number:
+                    return count
+            
         else:
-            predict_number = np.random.randint(91, 101)
-            if number == predict_number:
-                break  # Угадано   
-                
-    return count
+            predict_list = list(range(26, 50))
+            for predict_number in predict_list:
+                count += 1
+                if number == predict_number:
+                    return count
+        
+    # Идентично, если загаданное число больше 50
+    else:
+        predict_number = 75
+        count += 1
+        if number == predict_number:
+            return count
+            
+        elif number < predict_number:
+            predict_list = list(range(51, 75))
+            for predict_number in predict_list:
+                count += 1
+                if number == predict_number:
+                    return count
+            
+        else:
+            predict_list = list(range(76, 101))
+            for predict_number in predict_list:
+                count += 1
+                if number == predict_number:
+                    return count
 
-# Функция, которая показывает среднее количество попыток
+# Функция, которая возращает среднее количество попыток
 def score_game(game_predict) -> int:
     
     count_ls = []
-    np.random.seed(1)  # Фиксируем seed для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(10000))  # Загадываем список чисел
+    random_array = np.random.randint(1, 101, size=(1000))  # Загадываем список чисел
 
     for number in random_array:
         count_ls.append(game_predict(number))
